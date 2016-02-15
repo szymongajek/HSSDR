b2fa3c013cca5b7c3cc207c47036fc389f47db4b
szymon.gajek@gmail.com <szymon.gajek@gmail.com@b7ec931e-ea4c-4172-a838-725ccb255caa>
2016-02-15 11:25:55 +0000

    testy

---
+++ HSSDR-trunk/test_files/_test ont.pl
@@ -0,0 +1,13 @@
+
+communication(x) or technical(x)=> Rooms(x)
+
+
+success_msg "istnieja"
+failure_msg "nie istnieja"
+exists x,y in Rooms : x != y;
+
+
+#	Space,Kitchen,Corridor,Staircase,LiftConform,LiftNonConform,LiftCargo,
+#		Bathroom, ToiletMen,ToiletWomen, ToiledAccessible,Living_Room,Garage,Bedroom, 
+#		Office, Living, Communication, Technical,Server_Room,Storage, Private,Public,Empty
+		
